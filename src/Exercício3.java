422f96a467c356e07db074901e5441c4d6fb4086
Hugo <113729299+hugoferreiradev@users.noreply.github.com>
2022-10-18 21:17:56 -0300

    Update Exercício3.java

--- src/Exercício3.java
+++ src/Exercício3.java
@@ -1,13 +1,11 @@
-public class Exercício3{
-    
-    public static void main(String[] args) throws Exception {
-        int num1 = 10;
-        int num2 = 20;
-        int sucessor = num1 + 1;
-        int antecessor = num2 -1;
+import java.util.Scanner;
 
-        System.out.println("O primeiro número a ser exibido é: " + num1 + ", e o segundo é: " + num2);
-        System.out.println("O sucessor é: " + sucessor);
-        System.out.println("O antecessor é: " + antecessor);
+public class Exercício3 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Digite o número 1: ");
+        int n1 = scn.nextInt();
+        System.out.println("O antecessor de N1 é: " + (n1 -1)  + ", e seu sucessor é: " + (n1 + 1));
     }
 }
